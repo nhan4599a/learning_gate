5289a026475ea75abe3a5f91d06ff4433dc0e167
nhan <nhan0385790927@gmail.com>
2024-03-06 21:54:34 +0700

    [Add] Construct login page

--- src/app/authentication/sign-in/page.tsx
+++ src/app/authentication/sign-in/page.tsx
@@ -1,30 +1,46 @@
-'use client'
+"use client"
 
-import { Button, Form, Input, Space } from "antd"
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Checkbox, Divider } from "antd"
+import { LockOutlined, UserOutlined, GoogleOutlined } from "@ant-design/icons"
+import styles from "../../../styles/authentication.module.css"
+import FormItem from "antd/es/form/FormItem"
+import Link from "next/link"
 
 const Page = () => {
   const [form] = Form.useForm()
 
   return (
-    <Form form={form}>
-      <Form.Item name="Tên đăng nhập" label="Username/Email" required>
+    <Form form={form} className="login-form">
+      <h2>Log in to your account</h2>
+      <Form.Item name="Tên đăng nhập" label="Username" required>
         <Input prefix={<UserOutlined />} />
       </Form.Item>
       <Form.Item name="Mật khẩu" label="Password" required>
-        <Input
-          prefix={<LockOutlined />}
-          type="password"
-        />
+        <Input prefix={<LockOutlined />} type="password" />
       </Form.Item>
+
       <Form.Item>
-        <Space>
-          <Button type="primary" htmlType="submit" className="form-submit-button">
-            Đăng nhập
-          </Button>
-          Or <a href="">Đăng ký!</a>
-        </Space>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Link className={styles.forgot} href="/authentication/forgot-password">
+          Forgot Password?
+        </Link>
       </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button" block>
+          Log in  
+        </Button>
+        Don't have an account?<Link href="/authentication/sign-up"> Sign up</Link>
+      </Form.Item>
+      <Divider />
+      <FormItem>
+      <Button block>
+        <GoogleOutlined /> Log in with Google account
+      </Button>
+      </FormItem>
     </Form>
   )
 }
